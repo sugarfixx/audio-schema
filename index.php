6a3bd444c4697c1a407d432c46352aadabd88512
sugarfixx <ingar.torsrud@me.com>
2020-04-03 14:47:33 +0200

    Adds sample schema and data

--- index.php
+++ index.php
@@ -5,3 +5,7 @@
  * Date: 03/04/2020
  * Time: 11:27
  */
+require __DIR__ . '/vendor/autoload.php';
+
+$schema = (object)['$ref' => 'file://' . realpath('sample-data/schema.json')];
+$data = json_decode(file_get_contents('sample-data/data.json'));
--- src/AudioData.php
+++ src/AudioData.php
@@ -8,5 +8,18 @@
 
 class AudioData
 {
+    public function validate($schema, $data)
+    {
+        $validator = new JsonSchema\Validator;
+        $validator->validate($data, $schema);
 
+        if ($validator->isValid()) {
+            echo "The supplied JSON validates against the schema.\n";
+        } else {
+            echo "JSON does not validate. Violations:\n";
+            foreach ($validator->getErrors() as $error) {
+                echo sprintf("[%s] %s\n", $error['property'], $error['message']);
+            }
+        }
+    }
 }
