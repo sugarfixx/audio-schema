97ef316f28ad98e8c9edad0346a68cb5835b543e
sugarfixx <ingar.torsrud@me.com>
2020-04-03 14:53:25 +0200

    Adds tesable validation data

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
  * Time: 11:27
  */
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/src/AudioData.php';
 
 $schema = (object)['$ref' => 'file://' . realpath('sample-data/schema.json')];
 $data = json_decode(file_get_contents('sample-data/data.json'));
+
+$audioData = new AudioData();
+$audioData->validate($schema, $data);
