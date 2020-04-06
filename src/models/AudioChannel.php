f50c8b11b4feb51b0f319668155aa6448264b5e2
sugarfixx <ingar.torsrud@me.com>
2020-04-06 08:37:13 +0200

    Add models and updartes Language model

---
+++ src/models/AudioChannel.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 06/04/2020
+ * Time: 08:27
+ */
+
+class AudioChannel
+{
+
+}
---
+++ src/models/Language.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 06/04/2020
+ * Time: 08:27
+ */
+
+class Language
+{
+    /**
+     * @var String
+     */
+    public $iso_639_1;
+    /**
+     * @var String
+     */
+    public $iso_639_2;
+    /**
+     * @var String
+     */
+    public $rfc_5646;
+    /**
+     * @var String
+     */
+    public $name;
+    /**
+     * @var String
+     */
+    public $name_full;
+
+
+    public function set($key, $value)
+    {
+        $this->{$key} = $value;
+    }
+
+    public function get($key) {
+        return $this->{$key};
+    }
+
+}
