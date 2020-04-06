fa8c9f68fa3615d8474a931d718224b71f838d25
sugarfixx <ingar.torsrud@me.com>
2020-04-06 09:38:38 +0200

    Adds definitions to Audio stream

--- src/models/AudioStream.php
+++ src/models/AudioStream.php
@@ -6,7 +6,77 @@
  * Time: 08:26
  */
 
+
 class AudioStream
 {
+    /**
+     * @var integer
+     */
+    public $id;
 
+    /**
+     * @var integer
+     */
+    public $bit_depth;
+
+    /**
+     * @var integer
+     */
+    public $bit_rate;
+
+    /**
+     * @var string;
+     */
+    public $bit_rate_mode;
+
+    /**
+     * @var AudioChannel[]
+     */
+    public $channels = [];
+
+    /**
+     * @var string
+     */
+    public $codec_id;
+
+    /**
+     * @var string
+     */
+    public $endianess;
+
+    /**
+     * @var string
+     */
+    public $format;
+
+    /**
+     * @var string
+     */
+    public $sample_rate;
+
+    /**
+     * @var integer
+     */
+    public $stream_index;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $wrapping;
+
+
+    public function set($key, $value)
+    {
+        $this->{$key} = $value;
+    }
+
+    public function get($key)
+    {
+        return $this->{$key};
+    }
 }
