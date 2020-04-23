aa214a9b14c2f48d70a7bba7fc687c11910a17c9
sugarfixx <ingar.torsrud@me.com>
2020-04-23 16:23:55 +0200

    Updates model loader wirh models for vos package

--- src/models.php
+++ src/models.php
@@ -6,6 +6,8 @@
  * Date: 06/04/2020
  * Time: 10:27
  */
+
+// Audio Models
 require __DIR__ . '/models/Audio.php';
 require __DIR__ . '/models/AudioChannel.php';
 require __DIR__ . '/models/AudioMapping.php';
@@ -13,3 +15,8 @@ require __DIR__ . '/models/AudioStream.php';
 require __DIR__ . '/models/Language.php';
 require __DIR__ . '/models/MappedStream.php';
 require __DIR__ . '/models/Demo.php';
+
+// VodPackager Models
+require __DIR__ . '/models/VodPackage.php';
+require __DIR__ . '/models/SequenceItem.php';
+require __DIR__ . '/models/ItemClip.php';
