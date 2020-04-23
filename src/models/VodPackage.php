bb6adfd0ffe4aabb90f4ea6242f17f934c2a7a5f
sugarfixx <ingar.torsrud@me.com>
2020-04-23 16:20:58 +0200

    Adds model for VodPackage

---
+++ src/models/VodPackage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 23/04/2020
+ * Time: 16:19
+ */
+
+class VodPackage
+{
+    /**
+     * @var SequenceItem[]
+     */
+    public $sequences;
+}
